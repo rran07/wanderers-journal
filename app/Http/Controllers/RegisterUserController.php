878245e72284723ea7c8a1a62b2cfba0b6c73f06
Raniel Ronidel <r.ronidel0617@gmail.com>
2024-09-22 20:41:13 +0800

    i create a Seeder for User and also request-validate for sign up page but the data for now isn't storing in the database

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Validated;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
@@ -14,15 +18,12 @@ class RegisterUserController extends Controller
 
     public function store()
     {
-        $user = request()->validate([
-            'username' => '',
-            'first_name' => '',
-            'last_name' => '',
-            'email' => '',
-            'password' => '',
-            'password_confirmation' => '',
+        $validate = request()->validate([
+            'username' => ['required'],
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min('6'), 'confirmed'],
         ]);
-
-        dd($user);
     }
 }
