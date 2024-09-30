71f43a07ab65203920cca2c1d8b23540e373fe22
Raniel Ronidel <r.ronidel0617@gmail.com>
2024-10-01 01:59:54 +0800

    created a new design in login and signup: I add background image in login and update the color of the buttons and links into blue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,15 +2,16 @@
 
     <x-head />
 
-<body>
-    <div class="container mx-auto">
+<body class="bg-image-lg bg-cover bg-center">
+    <div class="mx-auto lg:max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <div class="flex justify-center items-center min-h-screen px-10 py-10">
-                <div class="flex h-heightCustom w-widthCustom flex-col justify-center px-6 py-12 lg:px-8 rounded-lg shadow-lg border">
+                <div class="flex h-heightCustom w-widthCustom flex-col justify-center px-6 py-12 lg:px-8 rounded-lg shadow-lg border bg-white/50">
                     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
                             alt="Your Company">
-                        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up to your
-                            account</h2>
+                        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                            Log In Your Account
+                        </h2>
                     </div>
 
                     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -22,7 +23,7 @@
                                     address</label>
                                 <div class="mt-2">
                                     <input id="email" name="email" type="email" autocomplete="email" required
-                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                 </div>
                             </div>
 
@@ -31,28 +32,28 @@
                                     <label for="password"
                                         class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                                     <div class="text-sm">
-                                        <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
+                                        <a href="#" class="font-semibold text-blue-600 hover:text-blue-500">Forgot
                                             password?</a>
                                     </div>
                                 </div>
                                 <div class="mt-2">
                                     <input id="password" name="password" type="password" autocomplete="current-password"
                                         required
-                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                 </div>
                             </div>
 
                             <div>
                                 <button type="submit"
-                                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                                    in</button>
+                                    class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+                                    Log In
+                                </button>
                             </div>
                         </form>
 
                         <p class="mt-10 text-center text-sm text-gray-500">
-                            Not a member?
-                            <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day
-                                free trial</a>
+                            Don't have account yet?
+                            <a href="/signup" class="font-semibold leading-6 text-blue-600 hover:text-blue-500">Sign up now!</a>
                         </p>
                     </div>
                 </div>
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -13,7 +13,7 @@
                         </div>
                         <div class="px-6 py-12 lg:px-8 rounded-md">
                             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                                <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
                                     alt="Your Company">
                                 <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up now!</h2>
                             </div>
@@ -26,21 +26,21 @@
                                         <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
                                         <div class="mt-2">
                                             <input id="username" name="username" type="username" autocomplete="username" required
-                                                class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
                                     <div class="pr-2">
                                         <label for="first_name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
                                         <div class="mt-2">
                                             <input id="first_name" name="first_name" type="first_name" autocomplete="first_name" required
-                                                class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
                                     <div>
                                         <label for="last_name" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
                                         <div class="mt-2">
                                             <input id="last_name" name="last_name" type="last_name" autocomplete="last_name" required
-                                                class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
                                     <div class="col-span-2">
@@ -48,7 +48,7 @@
                                             address</label>
                                         <div class="mt-2">
                                             <input id="email" name="email" type="email" autocomplete="email" required
-                                                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
 
@@ -61,7 +61,7 @@
                                         <div class="mt-2">
                                             <input id="password" name="password" type="password" autocomplete="current-password"
                                                 required
-                                                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
                                     <div>
@@ -73,20 +73,20 @@
                                         <div class="mt-2">
                                             <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password_confirmation"
                                                 required
-                                                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                                class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                                         </div>
                                     </div>
 
                                     <div class="col-span-2 pt-6">
                                         <button type="submit"
-                                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                                            class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign
                                             in</button>
                                     </div>
                                 </form>
 
                                 <p class="mt-10 text-center text-sm text-gray-500">
                                     Already have account?
-                                    <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Log in now!</a>
+                                    <a href="/login" class="font-semibold leading-6 text-blue-600 hover:text-blue-500">Log in now!</a>
                                 </p>
                             </div>
                         </div>
