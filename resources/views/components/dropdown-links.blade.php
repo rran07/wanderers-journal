af26ffac89b9a56bda19cca332555ae95609bde5
Raniel Ronidel <r.ronidel0617@gmail.com>
2024-10-14 21:43:11 +0800

    creating dropdown-links

--- resources/views/components/dropdown-links.blade.php
+++ resources/views/components/dropdown-links.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => "block px-4 py-2 mx-1 rounded-md text-sm font-semibold text-gray-700 hover:bg-gray-700/25"])}} role="menuitem" tabindex="-1">{{ $slot }}</a>
+<button {{ $attributes->merge(['class' => "block px-4 py-2 mx-1 rounded-md text-sm font-semibold text-gray-700 hover:bg-gray-700/25"])}} role="menuitem" tabindex="-1">{{ $slot }}</button>
--- resources/views/livewire/dropdown-menu.blade.php
+++ resources/views/livewire/dropdown-menu.blade.php
@@ -1,29 +1,28 @@
-<form action="/logout" method="POST">
-    @csrf
-    <div class="relative ml-3" x-data="{ isOpen: @entangle('isOpen') }">
-        <!-- Button -->
-        <div>
-            <button
-                wire:click="toggleDropdown"
-                type="button"
-                class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 hover:ring-2 hover:ring-white hover:ring-offset-2 hover:ring-offset-gray-800 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                <span class="absolute -inset-1.5"></span>
-                <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt="">
-            </button>
-        </div>
-
-        <!-- Dropdown -->
-        <div x-show="isOpen"
-            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <a id="user-menu-item-0">Your Profile</a>
-            <a id="user-menu-item-1">Settings</a>
-            <a id="user-menu-item-2">Sign out</a>
-        </div>
+<div class="relative ml-3" x-data="{ isOpen: @entangle('isOpen') }">
+    <!-- Button -->  
+    <div>
+        <button wire:click="toggleDropdown" type="button"
+            class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 hover:ring-2 hover:ring-white hover:ring-offset-2 hover:ring-offset-gray-800 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+            <span class="absolute -inset-1.5"></span>
+            <span class="sr-only">Open user menu</span>
+            <img class="h-8 w-8 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="">
+        </button>
     </div>
 
-</form>
+    <!-- Dropdown -->
+    <div x-show="isOpen"
+        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+        role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+        <x-dropdown-links onclick="window.location.href='/yourprofile'" id="user-menu-item-0">Your Profile
+        </x-dropdown-links>
+        <x-dropdown-links onclick="window.location.href='/settings'" id="user-menu-item-1">Settings</x-dropdown-links>
+        <form action="/logout" method="POST">
+            @csrf
+            <x-dropdown-links form="delete-form" onclick="window.location.href='/logout'" id="user-menu-item-2">Log out
+            </x-dropdown-links>
+        </form>
+    </div>
+</div>
